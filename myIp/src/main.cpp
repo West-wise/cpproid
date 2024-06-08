b8f91119ab9df1bef6587e424452ad1ad49c116b
PhoB <hsp003636@gmail.com>
2024-06-08 12:02:26 -0400

    Update : fix print format

--- myIp/src/main.cpp
+++ myIp/src/main.cpp
@@ -4,7 +4,6 @@
 #include "ip.h"
 #include "mac.h"
 
-
 int main(int argc, char *argv[])
 {
         if (argc <= 1)
@@ -13,13 +12,24 @@ int main(int argc, char *argv[])
         }
         char *dev = argv[1]; // 네트워크 인터페이스 명
         std::string dev_str = std::string(dev);
-        Ip ip = Ip::myIp(dev);
-        Mac mac = Mac::getMyMac(dev);
-        Ip GatewayIp = Ip::GatewayIp(dev_str);
-        printf("MY IP \t\t: %s\n", std::string(ip).c_str());
-        printf("Getway IP \t: %s\n", std::string(GatewayIp).c_str());
-        printf("MY MAC \t\t: %s\n", std::string(mac).c_str());
-        
+        Ip ip, GatewayIp;
+        Mac mymac;
+        try
+        {
+                ip = Ip::myIp(dev);
+                mymac = Mac::getMyMac(dev);
+                GatewayIp = Ip::GatewayIp(dev_str);
+        }
+        catch (const std::exception &e)
+        {
+                ip = Ip("0.0.0.0");
+                mymac = Mac::nullMac();
+                GatewayIp = Ip("0.0.0.0");
+        }
+
+        printf("Gateway_IP %s\n", std::string(GatewayIp).c_str());
+        printf("MY_IP %s\n", std::string(ip).c_str());
+        printf("MY_MAC %s\n", std::string(mymac).c_str());
 
         return 0;
 }
